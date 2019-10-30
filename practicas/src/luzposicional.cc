e817505a6663c29dc4fbad5de9ab2432590aacfd
Antonio David Villegas <advy99@gmail.com>
2019-10-30 18:39:31 +0100

    constructor luzposicional

---
+++ practicas/src/luzposicional.cc
@@ -0,0 +1,17 @@
+#include "luzposicional.h"
+
+LuzPosicional::LuzPosicional(const Tupla3f posicion,\
+                             const GLenum idLuzOpenGL, \
+                             const Tupla4f colorAmbiente,\
+                             const Tupla4f colorEspecular,\
+                             const Tupla4f colorDifuso){
+
+   id = idLuzOpenGL;
+   this.colorAmbiente = colorAmbiente;
+   this.colorDifuso = colorDifuso;
+   this.colorEspecular = colorEspecular;
+
+
+   this.posicion = {posicion(0), posicion(1), posicion(2), 1};
+
+}
