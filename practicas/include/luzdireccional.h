f3bcc4ad50a3feb872b3be7fba6beb896ebcbf41
Antonio David Villegas <advy99@gmail.com>
2019-10-30 19:16:32 +0100

    add constructor LuzDireccional, falta calcular alfa y beta segun la direccion y hacer los metodos de variacion de los angulos

--- practicas/include/luzdireccional.h
+++ practicas/include/luzdireccional.h
@@ -4,9 +4,17 @@
 
 class LuzDireccional : public Luz{
    public:
-      LuzDireccional(const Tupla2f direccion, const GLenum idLuzOpenGL, \
+      LuzDireccional(const Tupla3f direccion, const GLenum idLuzOpenGL, \
                      const Tupla4f colorAmbiente, const Tupla4f colorEspecular,\
                      const Tupla4f colorDifuso);
+
+      void variarAnguloAlpha(const float incremento);
+      void variarAnguloBeta(const floar incremento);
+
+   protected:
+      float alpha;
+      float beta;
+
 };
 
 
---
+++ practicas/src/luzdireccional.cc
@@ -0,0 +1,19 @@
+#include "luzdireccional.h"
+
+LuzDireccional::LuzDireccional(const Tupla3f direccion,\
+                               const GLenum idLuzOpenGL, \
+                               const Tupla4f colorAmbiente,\
+                               const Tupla4f colorEspecular,\
+                               const Tupla4f colorDifuso){
+
+   id = idLuzOpenGL;
+   this.colorAmbiente = colorAmbiente;
+   this.colorDifuso = colorDifuso;
+   this.colorEspecular = colorEspecular;
+
+
+   this.posicion = {direccion(0), direccion(1), direccion(2), 0};
+
+   //TO-DO: Calcular alpha y beta segun la dirección
+
+}
